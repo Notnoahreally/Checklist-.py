53b78ff07da22c6c2d09e702103f9d13eea64b4b
Notnoahreally <121029568+Notnoahreally@users.noreply.github.com>
2023-03-06 21:09:55 +0800

    Create Checklist.py

---
+++ Checklist.py
@@ -0,0 +1,61 @@
+l1 = input("Put list here: ")
+
+l2 = input("Put list 2 here: ")
+
+l3 = input("Put list 3 here: ")
+
+#Part 2
+
+print ("1.", l1)
+
+print ("2.", l2)
+
+print ("3.", l3)
+
+input ("Press Anything to continue")
+
+#Part 3
+
+print("Say 1=Yes, 2=No")
+
+A = input("Mark 1 Done: ")
+
+B = input("Mark 2 Done: ")
+
+C = input("Mark 3 Done: ")
+
+if A == '1':
+
+   resultA = "True"
+
+elif A == '2':
+
+    resultA = "False"
+
+if B == '1':
+
+   resultB = "True"
+
+elif B == '2':
+
+    resultB = "False"
+
+if C == '1':
+
+   resultC = "True"
+
+elif C == '2':
+
+    resultC = "False"
+
+print("List 1 Done:", resultA)
+
+print("List 2 Done:", resultB)
+
+print("List 3 Done:", resultC)
+
+   
+
+    
+
+   
